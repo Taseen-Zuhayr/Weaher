7243792b8f459b258b2321ba8163fae8017123cc
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-10-18 22:24:50 +0600

    Weather

---
+++ main.py
@@ -0,0 +1,7 @@
+def weather():
+    print("The weather is good in",x)
+    print("The weather is bad in",y)
+
+x = input("Enter your favourite weather : ")
+y = input("Enter your least favourite weather : ")
+weather()
